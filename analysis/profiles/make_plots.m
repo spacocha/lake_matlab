96d76542f4a9e3f5291618821a6e530617662751
Sarah Preheim <sprehei1@jhu.edu@compute0055.cm.cluster>
2017-07-18 11:08:00 -0400

    Changed plots to remove snrate value from name

--- analysis/profiles/make_plots.m
+++ analysis/profiles/make_plots.m
@@ -3,7 +3,7 @@
 % interactive run and plots each one on its own profile.
 
 % load up the data from interactive after writing it
-load('./data/history.mat')
+load('./plots/data/history.mat')
 conc_names = textread('../analysis/profiles/conc_names.txt', '%s', 'delimiter', '\n');
 rate_names = textread('../analysis/profiles/rate_names.txt', '%s', 'delimiter', '\n');
 depths = dlmread('../analysis/profiles/depths.txt');
@@ -18,20 +18,20 @@ colormap(map);
 
 % make each of the concs figures
 for i = 1: n_concs
-	  fn = sprintf('plots/end/sn%d_c_%s.pdf', snrate, conc_names{i});
+	  fn = sprintf('plots/end/c_%s.pdf', conc_names{i});
     plot(depths, concs_history(end, :, i));
     print(fn, '-dpdf');
 
-    fn = sprintf('plots/time/sn%d_c_%s.pdf', snrate, conc_names{i});
+    fn = sprintf('plots/time/c_%s.pdf', conc_names{i});
     make_ts_plot(concs_history(:, :, i), fn, 5, 100);
 end
 
 % make each of the rates figures
 for i = 1: n_rates
-	  fn = sprintf('plots/end/sn%d_r_%s.pdf', snrate, rate_names{i});
+	  fn = sprintf('plots/end/r_%s.pdf', rate_names{i});
     plot(depths, rates_history(end, :, i));
     print(fn, '-dpdf');
 
-    fn = sprintf('plots/time/sn%d_r_%s.pdf', snrate, rate_names{i});
+    fn = sprintf('plots/time/r_%s.pdf', rate_names{i});
     make_ts_plot(rates_history(:, :, i), fn, 5, 100);
 end
